beaf623961aaee818544ac7892e65956de50aeee
vnj64 <kamil_khabibullin@bk.ru>
2024-01-16 02:43:07 +0300

    style: fixed for flake8

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -5,15 +5,14 @@ import dotenv
 dotenv.load_dotenv()
 
 import pytest
+
 from app.tests.fixtures import create_db
 
 
 @pytest.fixture(scope="function")
 def db_session():
-    # Connect to your test database and create tables
     create_db()
 
     yield
 
-    # Tear down: Disconnect and drop the tables and delete the database file
     os.remove("test.db")
