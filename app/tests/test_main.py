71a1fb64832f16885942bd33f9a680235be4d8cb
vnj64 <kamil_khabibullin@bk.ru>
2024-01-16 01:07:52 +0300

    build: изменена логика тестирования

--- app/tests/test_main.py
+++ app/tests/test_main.py
@@ -1,23 +1,13 @@
 from fastapi.testclient import TestClient
-
+from fastapi import status
 from app.main import server
 
 client = TestClient(server)
 
 
-def test_register_user():
-    data = {"username": "testuser", "password": "testpassword"}
-    response = client.post("/register", data=data)
-    assert response.status_code == 200
-    assert response.json() == {"username": "testuser"}
-
-
-def test_login_and_get_token():
+def test_register():
     data = {"username": "testuser1", "password": "testpassword"}
-    response_register = client.post("/register", data=data)
-    assert response_register.status_code == 200
+    response = client.post("/register", data=data)
 
-    response_login = client.post("/token", data=data)
-    assert response_login.status_code == 200
-    assert "access_token" in response_login.json()
-    assert response_login.json()["token_type"] == "bearer"
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"username": "testuser1"}
