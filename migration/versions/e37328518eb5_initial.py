aa38d74bcb0df83fc030faa837a983a47f559349
vnj64 <kamil_khabibullin@bk.ru>
2024-01-16 01:08:41 +0300

    build: миграции актуальной БД

--- migration/versions/e37328518eb5_initial.py
+++ migration/versions/e37328518eb5_initial.py
@@ -7,12 +7,11 @@ Create Date: 2024-01-15 21:24:20.275539
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
-revision: str = 'e37328518eb5'
+revision: str = "e37328518eb5"
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
